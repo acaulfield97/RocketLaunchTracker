13c18d924a9866b1be174a04ada758dcfed9ff71
acaulfield97 <annacaulfield04@hotmail.com>
2024-06-25 13:42:09 +0100

    Render rocket icon markers on map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import styles from '../styles';
-// import Mapbox from '@rnmapbox/maps';
+import Mapbox, {
+  MapView,
+  LocationPuck,
+  Camera,
+  ShapeSource,
+  SymbolLayer,
+  Images,
+} from '@rnmapbox/maps';
+import {featureCollection, point} from '@turf/helpers';
+import rocket from '../components/media/icons/rocket_icon.png';
+import rockets from '../data/mockData.json';
 
 export default function MapScreen() {
+  const points = rockets.map(rocket =>
+    point([rocket.longitude, rocket.latitude]),
+  );
+  const rocketsFeatures = featureCollection(points);
   return (
-    <View style={styles.container}>
-      <Text>Map Screen</Text>
-    </View>
+    <MapView style={{flex: 1}}>
+      <Camera followUserLocation followZoomLevel={12} />
+      <LocationPuck
+        puckBearingEnabled
+        puckBearing="heading"
+        bearingImage="compass"
+      />
+      <ShapeSource id="rockets" shape={rocketsFeatures}>
+        <SymbolLayer
+          id="rockets-icons"
+          style={{iconImage: 'rocket', iconSize: 0.05, iconAllowOverlap: true}}
+        />
+        <Images images={{rocket}} />
+      </ShapeSource>
+    </MapView>
   );
 }
