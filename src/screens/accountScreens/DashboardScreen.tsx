5daf8b96fdd7af054c93cdf47fcf73e5de3c1910
acaulfield97 <annacaulfield04@hotmail.com>
2024-07-18 13:55:06 +0100

    Display data from database on dashboard (not dynamic)

--- src/screens/accountScreens/DashboardScreen.tsx
+++ src/screens/accountScreens/DashboardScreen.tsx
@@ -1,10 +1,38 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import {LaunchData} from '../../../types/types';
 
 const DashboardScreen = () => {
+  const [launch, setLaunch] = useState<LaunchData | null>(null);
+
+  const getData = async () => {
+    const launchDataCollection = await firestore()
+      .collection('launch_data')
+      .get();
+    console.log(launchDataCollection.docs[0].data());
+    setLaunch(launchDataCollection.docs[0].data() as LaunchData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View>
       <Text>Dashboard</Text>
+      {launch ? (
+        <>
+          <Text>Latitude: {launch.location.latitude}</Text>
+          <Text>Longitude: {launch.location.longitude}</Text>
+          <Text>Altitude: {launch.altitude}</Text>
+          <Text>
+            Time: {new Date(launch.time.seconds * 1000).toLocaleString()}
+          </Text>
+        </>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </View>
   );
 };
--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,14 @@
+import { GeoPoint, Timestamp } from "@react-native-firebase/firestore";
+
 export type RootStackParamList = {
-    BluetoothTerminal: undefined;
-    RawData: { parsedData: any[] };
-    CreateAccount: undefined;
-    Login: undefined;
-  };
+  BluetoothTerminal: undefined;
+  RawData: { parsedData: any[] };
+  CreateAccount: undefined;
+  Login: undefined;
+};
+
+export type LaunchData = {
+  location: GeoPoint,
+  altitude: number,
+  time: Timestamp
+}
